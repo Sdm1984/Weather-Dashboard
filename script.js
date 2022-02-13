d1b4263a5fd92942eb365b519ad38c81d014fe50
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-02-12 20:46:58 -0500

    add nav bar code

--- script.js
+++ script.js
@@ -2,3 +2,35 @@
 
 const APIKey ="314ab9d4b0b9be7593d4189119cf468b";
 
+//Weather App - Pseudocode 
+
+//SEARCH
+//Function: EVENT HANDLER
+//1. Read the value from the text box in a variable – create a var and read the value of the textbox into the var
+//2. Create Url for Fetch
+//3. Fetch the results
+//4. Call Function to update after results
+
+fetch(url)
+
+//Function: DoResults
+//1. Call function Add Search Term to Results
+//2. Call function Add Today’s Results
+//3. Call function Add 5 days Results
+//4. Clear out the text box
+
+
+//RECENT SEARCH
+//Function: Add Search Term to Results- after user performs a search, I need to add a function to adding the search term to results.
+//1. Take a Parameter In: Search Term
+//1. Add a button to the screen, with data attribute that stores the search term
+
+//Function: EVENT HANDLER (for recent search buttons)
+//1. Read the data attribute to get the search term - user should be able to click on each one and see current data
+//2. Call Search function
+
+//RESULTS TODAY
+//Function: Add Today’s Results
+
+//RESULTS 5 days
+//Function: Add 5 days Results
